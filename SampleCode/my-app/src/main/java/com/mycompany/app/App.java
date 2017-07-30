1daeee3b5ee9651737779228fce71163543c494f
Paige Peck <paigepeck@hotmail.com>
2017-07-30 16:32:55 -0400

    Added project to git

---
+++ SampleCode/my-app/src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,16 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Enter a blog entry: ");
+        String entry = reader.nextString();
+
+    }
+}
